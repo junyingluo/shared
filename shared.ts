bd177dc73fd13847213fb53c37ee8aad56d71198
luojunying <luojunying@bytedance.com>
2019-07-29 01:07:34 +0800

    feat: update

--- shared.ts
+++ shared.ts
@@ -2,48 +2,48 @@ interface IHandler {
   [index: string]: (param: any, update: Function, dispatch: Function) => void;
 }
 
-interface IUpdater {
+interface IListener {
   [index: string]: (param: any) => void;
 }
 
 class Shared {
-  updaters: IUpdater[] = [];
-  data = { update: {}, dispatch: {} };
+  listeners: IListener[] = [];
   handler: IHandler = {};
+  cache = { data: {}, dispatch: {} };
 
   constructor(param: IHandler) {
     this.handler = param;
   }
 
   update(action: string, param: any) {
-    if (this.data.update[action] == param) return;
-    this.data.update[action] = param;
-    this.updaters.map(func => {
-      if (this.data[action] && func[action]) {
-        func[action](this.data.update[action]);
+    if (this.cache.data[action] == param) return;
+    this.cache.data[action] = param;
+    this.listeners.map(listener => {
+      if (this.cache[action] && listener[action]) {
+        listener[action](this.cache.data[action]);
       }
     });
   }
 
   dispatch(action: string, param: any) {
-    if (this.data.dispatch[action] == param) return;
-    this.data.dispatch[action] = param;
+    if (this.cache.dispatch[action] == param) return;
+    this.cache.dispatch[action] = param;
     if (this.handler[action]) {
       this.handler[action](param, this.update, this.dispatch);
     }
   }
 
-  register(updater: IUpdater) {
-    if (updater) {
-      this.updaters.push(updater);
+  register(listener: IListener) {
+    if (listener) {
+      this.listeners.push(listener);
     }
   }
 
-  unregister(updater: IUpdater) {
-    if (updater) {
-      const i = this.updaters.indexOf(updater);
+  unregister(listener: IListener) {
+    if (listener) {
+      const i = this.listeners.indexOf(listener);
       if (i >= 0) {
-        this.updaters.splice(i, 1);
+        this.listeners.splice(i, 1);
       }
     }
   }

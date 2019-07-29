7352e4ea720009caed48051a103b4b9e7da9fd5a
luojunying <luojunying@bytedance.com>
2019-07-29 14:00:24 +0800

    fix: update

--- shared.ts
+++ shared.ts
@@ -3,50 +3,50 @@ interface IHandler {
 }
 
 interface IListener {
-  [index: string]: (param: any) => void;
+  [index: string]: (param?: any) => void;
 }
 
 class Shared {
   listeners: IListener[] = [];
-  handler: IHandler = {};
   cache = { data: {}, dispatch: {} };
+  handler: IHandler = {};
 
   constructor(param: IHandler) {
     this.handler = param;
   }
 
-  update(action: string, param: any) {
+  update = (action: string, param: any) => {
     if (this.cache.data[action] == param) return;
     this.cache.data[action] = param;
     this.listeners.map(listener => {
-      if (this.cache[action] && listener[action]) {
+      if (this.cache.data[action] && listener[action]) {
         listener[action](this.cache.data[action]);
       }
     });
-  }
+  };
 
-  dispatch(action: string, param: any) {
+  dispatch = (action: string, param: any) => {
     if (this.cache.dispatch[action] == param) return;
     this.cache.dispatch[action] = param;
     if (this.handler[action]) {
       this.handler[action](param, this.update, this.dispatch);
     }
-  }
+  };
 
-  register(listener: IListener) {
+  register = (listener: IListener) => {
     if (listener) {
       this.listeners.push(listener);
     }
-  }
+  };
 
-  unregister(listener: IListener) {
+  unregister = (listener: IListener) => {
     if (listener) {
       const i = this.listeners.indexOf(listener);
       if (i >= 0) {
         this.listeners.splice(i, 1);
       }
     }
-  }
+  };
 }
 
 export function createShared(handler: IHandler) {
